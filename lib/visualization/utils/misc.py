712baa62577f8e1cb0e6a4f3d37625fc36533600
Marcin Kalinowski <kalmar@linux-a4lp.site>
2015-04-25 20:04:50 +0200

    fix pipe reading

--- lib/visualization/utils/misc.py
+++ lib/visualization/utils/misc.py
@@ -3,6 +3,8 @@ __author__ = 'kalmar'
 import string
 import random
 import os
+import subprocess
+import threading
 from PyQt5.QtCore import QObject, pyqtSignal, QThread, QProcess
 from select import select
 
@@ -28,16 +30,33 @@ class BlockingFIFOReader(QObject):
         # See http://www.outflux.net/blog/archives/2008/03/09/using-select-on-a-fifo/
         # We also use os.open and os.fdopen rather than built-in open so we can
         # add O_NONBLOCK.
-        oshandle = os.open(self.filename, os.O_RDWR | os.O_NONBLOCK)
-        self.fifo = os.fdopen(oshandle, 'r')
+        print(self.filename)
+        self.fifo = open(self.filename, "r")
         while True:
-            print("thread loop")
             ready_r, ready_w, ready_e = select([self.fifo], [], [], 1)
             if ready_r:
-                print("reading data")
                 lines = self.fifo.readlines()
                 for line in lines:
                     self.got_line.emit(line.rstrip())
             if QThread.currentThread().isInterruptionRequested():
                 self.finished.emit()
                 return
+
+
+class PopenAndCall(QObject):
+
+    finished = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        super(PopenAndCall, self).__init__()
+
+    def start(self):
+        threading.Thread(target=self.runInThread, args=[]).start()
+
+    def runInThread(self):
+        proc = subprocess.Popen(*self.args, **self.kwargs)
+        proc.wait()
+        self.finished.emit()
+    # returns immediately after the thread starts
