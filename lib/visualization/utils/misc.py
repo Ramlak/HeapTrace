90dab7ed6c01ba0b589a451f0de72939eead4566
Marcin Kalinowski <kalmar@linux-a4lp.site>
2015-05-03 00:03:50 +0200

    add multiple connecting tries

--- lib/visualization/utils/misc.py
+++ lib/visualization/utils/misc.py
@@ -1,8 +1,10 @@
 from Queue import Queue, Empty
-from _socket import AF_INET
+from _socket import error
 import shlex
 from socket import socket
-from utils.heap import cmd_type_t, heap_op_packet_t_32, idacmd_packet_t_64, idacmd_packet_t_32, heap_op_packet_t_64
+from time import time, sleep
+from utils.heap import cmd_type_t, heap_op_packet_t_32, idacmd_packet_t_64, idacmd_packet_t_32, heap_op_packet_t_64, \
+    CMD_TYPE_T_NAMES
 
 __author__ = 'kalmar'
 
@@ -12,17 +14,13 @@ import os
 import subprocess
 import threading
 from settings import settings, ROOT_DIR
-from PyQt5.QtCore import QObject, pyqtSignal, QThread, QProcess
+from PyQt5.QtCore import QObject, pyqtSignal
 
 
-def getcmd():
+def getcmd(bits):
     final_cmd = ""
-    if int(settings.get('new trace', 'BITS')) == 32:
-        pin_cmd = "pin -t {} -o {} -- {}".format(os.path.join(ROOT_DIR, "lib/pin/obj-ia32/heaptrace.so"), settings.get('new trace', 'EXECUTABLE'))
-    elif int(settings.get('new trace', 'BITS')) == 64:
-        pin_cmd = "pin -t {} -o {} -- {}".format(os.path.join(ROOT_DIR, "lib/pin/obj-intel64/heaptrace.so"), settings.get('new trace', 'EXECUTABLE'))
-    else:
-        raise Exception("WTF!?")
+    pintool_path = os.path.join(ROOT_DIR, "lib/pintool/obj-{}/heaptrace.so".format(["", "ia32", "intel64"][bits/32]))
+    pin_cmd = "pin -t {} -d 2 -p {} -- {}".format(pintool_path, int(settings.get('main', 'PIN_SERVER_PORT')),  settings.get('new trace', 'EXECUTABLE'))
 
     if settings.get('new trace', 'USE_TCP_WRAPPER') != 'False':
         final_cmd += settings.get('main', 'TCP_WRAPPER_COMMAND').replace('[CMD]', pin_cmd).replace('[PORT]', settings.get('new trace', 'TCP_WRAPPER_PORT'))
@@ -43,27 +41,21 @@ class PinCommunication(QObject):
     got_heap_op = pyqtSignal(str)
     finished = pyqtSignal()
 
-    def __init__(self, host, port, bits):
+    def __init__(self, host, port, bits, events):
         super(PinCommunication, self).__init__()
+        self.host, self.port, self.bits = host, port, bits
         self.s = socket()
-        self.events = Queue(maxsize=0)
-        self.set_bitness(bits)
-        try:
-            self.s.connect((host, port))
-        except Exception:
-            import traceback
-            traceback.print_exc()
-            exit(1)
-        self.run_loop = True
+        self.events = events
+        self.set_bitness()
+        self.run_loop = False
 
     def sendall(self, data):
-        return self.s.sendall(data)
+        self.s.sendall(data)
 
     def recv(self, nb):
         return self.s.recv(nb)
 
-    def set_bitness(self, bits):
-        self.bits = bits
+    def set_bitness(self):
         if self.bits == 32:
             self.idacmd_packet_t = idacmd_packet_t_32
             self.heap_op_packet_t = heap_op_packet_t_32
@@ -73,16 +65,54 @@ class PinCommunication(QObject):
         else:
             print "Wtf bits are wrong!!"
 
+    def connect(self):
+        print "Trying to connect"
+        count = time()
+        while True:
+            sleep(0.5)
+            try:
+                self.s.connect((self.host, self.port))
+                self.run_loop = True
+                self.events.put(cmd_type_t.CTT_HELLO)
+                self.events.put(cmd_type_t.CTT_START_PROCESS)
+                print "Connected!"
+                break
+            except error as e:
+                if error.errno == 111:
+                    if time() - count < 10000:
+                        print time() - count
+                        continue
+                    else:
+                        print "Cannot connect"
+                        break
+                else:
+                    import traceback
+                    traceback.print_exc()
+                    break
+
     def event_loop(self):
+            self.connect()
             while self.run_loop:
                 try:
-                    self.handle_event(self.events.get(timeout=1))
+                    print "Entering try,except"
+                    x = self.events.get(block=0, timeout=1)
+                    print CMD_TYPE_T_NAMES[x]
+                    self.handle_event(x)
                     self.events.task_done()
+                    print "End of try - task done"
                 except Empty:
-                    self.events.put(cmd_type_t.CTT_GET_HEAP_OP)
+                    print "Queue empty"
+                    self.events.put(cmd_type_t.CTT_CHECK_HEAP_OP)
+            self.finished.emit()
 
     def get_ans(self):
         return self.recv(1024)
+
+    def fill_cmd(self, bytes):
+        return self.idacmd_packet_t().fill(bytes)
+
+    def fill_op(self, bytes):
+        return self.heap_op_packet_t().fill(bytes)
 
     def send_cmd(self, **kwargs):
         self.sendall(self.idacmd_packet_t(**kwargs))
@@ -95,9 +125,13 @@ class PinCommunication(QObject):
             self.get_heap_op()
         elif event == cmd_type_t.CTT_HELLO:
             self.hello()
+        elif event == cmd_type_t.CTT_START_PROCESS:
+            self.start_process()
 
     def check_heap_op(self):
-        left = self.idacmd_packet_t().fill(self.send_cmd(code=cmd_type_t.CTT_CHECK_HEAP_OP)).size
+        print "CHECK HEAP OP"
+        left = self.fill_cmd(self.send_cmd(code=cmd_type_t.CTT_CHECK_HEAP_OP)).size
+        print "%d heap operations found" % left
         while left > 0:
             self.events.put(cmd_type_t.CTT_GET_HEAP_OP)
             left -= 1
@@ -106,11 +140,18 @@ class PinCommunication(QObject):
         self.got_heap_op.emit(str(self.send_cmd(code=cmd_type_t.CTT_GET_HEAP_OP)))
 
     def hello(self):
+        ans = self.fill_cmd(self.send_cmd(code=cmd_type_t.CTT_HELLO))
+        if ans.size*8 != self.bits:
+            self.run_loop = False
+
+    def start_process(self):
+        self.send_cmd(code=cmd_type_t.CTT_START_PROCESS)
 
 
 class PopenAndCall(QObject):
 
     finished = pyqtSignal()
+    started = pyqtSignal()
 
     def __init__(self, *args, **kwargs):
         self.args = args
@@ -125,7 +166,8 @@ class PopenAndCall(QObject):
         cwd = os.getcwd()
         os.chdir(settings.get('new trace', 'START_DIR'))
         proc = subprocess.Popen(*self.args, **self.kwargs)
-        os.chdir(cwd)
         self.heaptrace.proc = proc
+        self.started.emit()
+        os.chdir(cwd)
         proc.wait()
         self.finished.emit()
