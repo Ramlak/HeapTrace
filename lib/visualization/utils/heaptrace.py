993f78b93a302b4dac70fcddfe06822f9661c13a
Marcin Kalinowski <kalmar@linux-a4lp.site>
2015-05-03 00:02:55 +0200

    add signal messaging: start thread on process start

--- lib/visualization/utils/heaptrace.py
+++ lib/visualization/utils/heaptrace.py
@@ -1,6 +1,7 @@
+from Queue import Queue
 from PyQt5.QtCore import QThread
-import os
-from utils.misc import randoms, getcmd, PopenAndCall, PinCommunication
+from settings import settings
+from utils.misc import getcmd, PopenAndCall, PinCommunication
 
 __author__ = 'kalmar'
 
@@ -14,47 +15,50 @@ class HeapTrace(object):
 
     def run(self):
         self.log = []
+        self.bits = int(settings.get('new trace', 'BITS'))
         self.mainWindow.textLog.clear()
-        self.reader = PinCommunication('localhost', 12345)
-        self.thread = QThread()
-        self.reader.moveToThread(self.thread)
-        self.reader.got_heap_op.connect(self.newHeapOperation)
-        self.thread.started.connect(self.reader.read_fifo)
-        self.reader.finished.connect(self.on_reader_finished)
-        self.thread.finished.connect(self.on_thread_finished)
-        invocation = PopenAndCall(getcmd(self.fifopath), shell=False)
+        invocation = PopenAndCall(getcmd(self.bits), shell=False)
         invocation.finished.connect(self.on_proc_finished)
+        invocation.started.connect(self.on_proc_started)
         invocation.start(self)
-        self.thread.start()
 
     def kill(self):
+        print "Kill them all!"
         if self.thread:
             self.thread.quit()
         if self.proc:
-            self.proc.kill()
+            print "Terminating process!"
+            self.proc.terminate()
 
     def newHeapOperation(self, line):
         self.log.append(line)
         self.mainWindow.textLog.append(line)
 
+    def on_proc_started(self):
+        print "Started process.."
+        self.mainWindow.status("Process started")
+        self.events = Queue(maxsize=0)
+        self.reader = PinCommunication('localhost', 12345, self.bits, self.events)
+        self.thread = QThread()
+        self.reader.moveToThread(self.thread)
+        self.reader.got_heap_op.connect(self.newHeapOperation)
+        self.thread.started.connect(self.reader.event_loop)
+        self.reader.finished.connect(self.on_reader_finished)
+        self.thread.finished.connect(self.on_thread_finished)
+        self.thread.start()
+
     def on_proc_finished(self):
+        if self.thread:
+            self.thread.quit()
         self.proc = None
         self.mainWindow.status("Process finished ({} lines)".format(len(self.log)))
-        self.thread.requestInterruption()
-        try:
-            os.remove(self.fifopath)
-        except Exception:
-            pass
 
     def on_reader_finished(self):
+        print "Reader finished"
         self.kill()
-        self.reader.fifo.close()
         self.reader.deleteLater()
-        try:
-            os.remove(self.fifopath)
-        except Exception:
-            pass
 
     def on_thread_finished(self):
+        print "Thread finished"
         self.thread.deleteLater()
         self.thread = None
